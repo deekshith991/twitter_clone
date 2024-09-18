376be712c9a7dac6e1209366af315748228e54d6
vaggu Deekshith <vaggudeekshith@oulook.com>
2024-09-18 15:03:28 +0530

    Basic config

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,17 @@
 
 console.log("[+] Booting the SERVER ");
 
+const express = require('express');
+const env = require('dotenv');
+const mongoose = require('mongoose');
+const cors = require('cors');
 
+
+// configurations
+env.config();
+const PORT = 3333;
+const MongoUri = "mongodb://localhost:27017/twitter/";
+
+// app configurations
+const app = express();
+app.use(express.json());
